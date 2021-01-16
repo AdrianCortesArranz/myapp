ec380c276c3b2a5baf5999e032878b4e99ed1604
AdrianCortesArranz <adrian.cortes.work@gmail.com>
2021-01-16 19:00:07 +0000

    inicial

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+from flask import jsonify
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello, World! por fin funciona esta mierda'
+    message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+    response = {
+            " message": message,
+            " length": len(message)
+    }
+    return jsonify(response)
 
 @app.route('/bye')
 def bye__world():
