4d647312df5df9faed299dd847cbf86ad7be51f5
AdrianCortesArranz <adrian.cortes.work@gmail.com>
2021-01-16 18:35:38 +0000

    inicial

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! por fin funciona esta mierda'
 
 @app.route('/bye')
 def bye__world():
-    return 'bye, World!'
+    return 'bye, World! me voy de la fucking live '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
